a0ac03d0a5a57f102f8f343578cb31d8bfe9cf29
baebaebaeh <bae9954@naver.com>
2024-09-05 12:49:14 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main (String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "");
+        bw.flush();
+    }
+}
