481b0b3ebaeb100bfab840a1b67a8735668db4cb
baehanjin <bae9954@naver.com>
2025-02-03 19:24:27 +0900

    123

---
+++ codetree/src/main/java/com/ssafy/medusawarrior/Main.java
@@ -0,0 +1,17 @@
+package com.ssafy.medusawarrior;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("data/medusawarrior.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
